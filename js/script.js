87065ae0c5c9571bd3cec951491747c0dbe1cc60
Darisam <98291008+Darisam@users.noreply.github.com>
2022-03-10 11:14:53 +0100

    REmove old way of adding pokemon to list

--- js/script.js
+++ js/script.js
@@ -1,19 +1,4 @@
 
-/* This function tests whether two arrays are the same in terms
-of length and content. */
-
-function arrayCompare(array1, array2) {
-  if (Array.isArray(array1) !== true ||
-  Array.isArray(array2) !== true ||
-  array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
 function capitalizeFirst(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
@@ -30,8 +15,6 @@ function heightInFeet(heightInInches) {
 const pokemonRepository = (function() {
 
   const pokemonList = [];
-  const keyTemplate =
-  ['name', 'category', 'height', 'weight', 'type', 'weakness', 'stats'];
   const apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
 
@@ -42,21 +25,6 @@ const pokemonRepository = (function() {
   function getAll() {
     return JSON.parse(JSON.stringify(pokemonList));
   }
-
-  function add(pokemon) {
-    if (typeof pokemon === 'object') {
-      if (arrayCompare(keyTemplate, Object.keys(pokemon)) === true) {
-        pokemonList.push(pokemon);
-      }
-      else {
-        console.log(pokemon.name === undefined ?
-          'pokemonRepository.add: Item is not a proper object or lacks a name key.'
-          : 'pokemonRepository.add: ' + pokemon.name +
-          ' is not a properly formated pokemon.' );
-        }
-      }
-      else console.log('pokemonRepository.add: Type of item is not object.');
-    }
 
     // This function acts as a test function in the getByName function.
 
@@ -74,109 +42,6 @@ const pokemonRepository = (function() {
       add: add
     };
   })();
-
-  /* Adding some raw data to the repository so we have something to work with,
-  plus a whole lot of dummys to check the layout of the list */
-
-  pokemonRepository.add(
-    {
-      name: 'Bulbasaur',
-      category: 'Seed',
-      height: 28,
-      weight: 15.2,
-      type: ['Grass', 'Poison'],
-      weakness: ['Fire', 'Psychic', 'Flying', 'Ice'],
-      stats: {
-        hp: 3,
-        attack: 3,
-        defense: 3,
-        specialAttack: 4,
-        specialDefense: 4,
-        speed: 3
-      }
-    }
-  );
-
-  pokemonRepository.add(
-    {
-      name: 'Ivysaur',
-      category: 'Seed',
-      height: 39,
-      weight: 28.7,
-      type: ['Grass', 'Poison'],
-      weakness: ['Fire', 'Psychic', 'Flying', 'Ice'],
-      stats: {
-        hp: 4,
-        attack: 4,
-        defense: 4,
-        specialAttack: 5,
-        specialDefense: 5,
-        speed: 4
-      }
-    }
-  );
-
-  pokemonRepository.add(
-    {
-      name: 'Venusaur',
-      category: 'Seed',
-      height: 79,
-      weight: 220.5,
-      type: ['Grass', 'Poison'],
-      weakness: ['Fire', 'Psychic', 'Flying', 'Ice'],
-      stats: {
-        hp: 5,
-        attack: 5,
-        defense: 5,
-        specialAttack: 6,
-        specialDefense: 6,
-        speed: 5
-      }
-    }
-  );
-
-  pokemonRepository.add(
-    {
-      name: 'Charmander',
-      category: 'Lizard',
-      height: 24,
-      weight: 18.7,
-      type: ['Fire'],
-      weakness: ['Water', 'Ground', 'Rock'],
-      stats: {
-        hp: 3,
-        attack: 4,
-        defense: 3,
-        specialAttack: 4,
-        specialDefense: 3,
-        speed: 4
-      }
-    }
-  );
-
-  function createDummy() {
-    return {
-      name: 'Dummy',
-      category: '',
-      height: 0,
-      weight: 0,
-      type: [],
-      weakness: [],
-      stats: {
-        hp: 0,
-        attack: 0,
-        defense: 0,
-        specialAttack: 0,
-        specialDefense: 0,
-        speed: 0}
-      }
-    }
-
-    for (let i = 0; i < 40; i++) {
-      pokemonRepository.add(createDummy());
-    }
-
-
     // Html output section
 
     // Create the functionality of the hamburger menu
